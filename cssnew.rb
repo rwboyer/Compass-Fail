445b8bb5533f8542f19fa524553aee28c5c4f618
RWB <rwboyer@photo.rwboyer.com>
2009-09-06 11:42:19 -0400

    converted auth to gem

--- cssnew.rb
+++ cssnew.rb
@@ -1,29 +1,17 @@
-%w( rubygems sinatra compass rest_client hpricot json ).each {|f| require f}
+%w( rubygems sinatra compass rest_client hpricot json sinatra-authentication).each {|f| require f}
 
 use Rack::Session::Cookie, :secret => 'WTF'
+
+set :rpxapikey, '45ba29026c158111481c53d20dd27fead98130f1'
+set :rpxappname, 'compassfail'
+set :rpxserver, '192.168.1.101:4567'
+set :rpxadmins, { 'rwboyer.aperture' => 'admin' }
 
 #Setup sinatra to use Compass with the correct directorys
 configure do
 	Compass.configuration do |conf|
 		conf.project_path = File.dirname(__FILE__)
 		conf.sass_dir = File.join('views', 'stylesheets')
-	end
-end
-
-def authenticate(token)
-	response = JSON.parse(RestClient.post 'https://rpxnow.com/api/v2/auth_info', :token => token, 'apiKey' => '45ba29026c158111481c53d20dd27fead98130f1', :format => 'json', :extended => 'false')
-	puts response.inspect
-	return response if response['stat'] == 'ok'
-	return nil
-end
-
-def auth_required
-	if session[:user]
-		return true
-	else
-		session[:return_to] = request.fullpath
-		redirect '/'
-		return false
 	end
 end
 
@@ -35,23 +23,8 @@ get "/stylesheets/screen.css" do
 	sass :"stylesheets/screen", Compass.sass_engine_options
 end
 
-#bogus - this is not how rpx open-id works
-get "/login" do
-	'You are logged in!' if authenticate(params[:token])
-end
-
-#this is actually what you need
-post "/login" do
-	r = nil
-	session[:user] = r
-	session[:user] = r if r = authenticate(params[:token])
-	puts r.inspect
-	puts session[:user].inspect
-	redirect '/'
-end
-
 get "/form" do
-	auth_required
+	login_required('/fail')
 	haml :form
 end
 
@@ -62,7 +35,7 @@ post "/form" do
 end
 
 get "/dorss" do
-	auth_required
+	priv_required('admin', '/fail')
 	resp = RestClient.get 'http://photo.rwboyer.com/feed/'
 	doc, @posts = Hpricot::XML(resp), [] 
 	puts doc.inspect
@@ -73,6 +46,10 @@ get "/dorss" do
 	end
 	puts @posts.inspect
 	haml :rsspage
+end
+
+get "/fail" do
+	"Authorization Failure"
 end
 
 get "/" do
